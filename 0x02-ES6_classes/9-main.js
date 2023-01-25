8669d07445e362c1c24d988e169b33edace71a51
vantwill <vtwill41@gmail.com>
2023-01-25 10:43:32 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/9-main.js
+++
@@ -1,9 +0,0 @@
-import listOfStudents from "./9-hoisting.js";
-
-console.log(listOfStudents);
-
-const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
-);
-
-console.log(listPrinted)
